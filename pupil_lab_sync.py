f69db576898b4f3195835172720a0f33a9bc74ad
Seth Foster <fosterseth@users.noreply.github.com>
2017-05-24 18:15:34 -0400

    newer version of pupil lab sync, not using ffmpeg concat demuxer

--- pupil_lab_sync.py
+++ pupil_lab_sync.py
@@ -9,38 +9,47 @@ def main():
     
     frame_time = 1 / 30.0
 
-    tmp1 = output_filename + ".tmp"
+    tmp1 = output_filename + ".tmp1"
     if not os.path.exists(tmp1):
         os.mkdir(tmp1)
 
-    command = "ffmpeg -i " + video_filename + " -f image2 -qscale:v 5 " + tmp1 + "\%d.jpg"
+    command = "ffmpeg -i " + video_filename + " -f image2 -qscale:v 2 " + tmp1 + "\%d.jpg"
     print(command)
     subprocess.call(command.split(' '))
     
-    # tmp2 = output_filename + ".tmp2"
-    # if not os.path.exists(tmp2):
-        # os.mkdir(tmp2)
+    tmp2 = output_filename + ".tmp2"
+    if not os.path.exists(tmp2):
+        os.mkdir(tmp2)
         
     timestamps = numpy.fromfile(timestamps_filename, dtype='>f8')
     timestamps.tofile(output_timestamps_filename, "\n", "%s")
     timestamps = timestamps - timestamps[0]
     curr_time = 0.0
     curr_image = 1
-    fid = open(output_filename + '.ffconcat', 'w')
-    fid.write("ffconcat version 1.0\n")
+
+    previdx = -1
+    prevdest = "-1"
     while curr_time <= timestamps[-1]:
         idx = numpy.argmin(numpy.absolute(timestamps-curr_time))
         curr_time += frame_time
-        fid.write("file %s/%d.jpg" % (tmp1, (idx+1)) + "\n")
-        fid.write("duration 0.033\n")
-        # src = tmp1 + "\\" + "%d.jpg" % (idx+1)
-        # dest = tmp2 + "\\" + "%d.jpg" % curr_image
+        if idx == previdx:
+            src = prevdest
+            movefile = False
+        else:
+            src = tmp1 + "\\" + "%d.jpg" % (idx+1)
+            movefile = True
+        dest = tmp2 + "\\" + "n_%d.jpg" % curr_image
+        if curr_image < 10:
+            print(src, dest, movefile)
+        prevdest = dest;
+        previdx = idx;
         curr_image += 1
-        # shutil.copy(src, dest)
-    fid.write("file %s/%d.jpg" % (tmp1, (idx+1)) + "\n")
-    fid.close()
+        if movefile:
+            shutil.move(src, dest)
+        else:
+            shutil.copy(src, dest)
         
-    command = "ffmpeg -f concat -i " + output_filename + ".ffconcat -pix_fmt yuv420p -vcodec mpeg4 -qscale:v 8 -r 30 " + output_filename
+    command = "ffmpeg -framerate 30 -i " + tmp2 + "\\n_%d.jpg -pix_fmt yuv420p -vcodec mpeg4 -qscale:v 2 -r 30 " + output_filename
     subprocess.call(command.split(' '))
 
 if __name__ == "__main__":
